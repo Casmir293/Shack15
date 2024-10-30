20bc497cfab07fb8ed7f57a33409f455fd5c60cf
Casmir Onyeka <casmir293@gmail.com>
2024-10-31 00:35:20 +0100

    feat: added metrics logic.

---
+++ Shack15-Backend/app/Models/Metrics.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Metrics extends Model
+{
+    use HasFactory;
+
+    protected $table = 'metrics';
+
+    // Allow mass assignment for these fields
+    protected $fillable = [
+        'total_members',
+        'active_members',
+        'events',
+    ];
+
+    // Cast events to an array when retrieved
+    protected $casts = [
+        'events' => 'array',
+    ];
+}
---
+++ Shack15-Backend/database/migrations/2024_10_30_221655_create_metrics_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('metrics', function (Blueprint $table) {
+            $table->id();
+            $table->integer('total_members');
+            $table->integer('active_members');
+            $table->json('events');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('metrics');
+    }
+};
